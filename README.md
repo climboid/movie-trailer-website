a4dae1c82db70f2f2a1cd632a8ffa4f512fdb2c7
Oscar Villarreal <oscarvillarreal14@gmail.com>
2015-05-09 08:22:07 +0200

    updating code after review

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # movie-trailer-website
 
 Simple interface that shows a list of movies and their trailes by clicking on a movie.
-Built mainly in python
+Built mainly in python.
+
+## Running the program
+
+You will need python 2.7.9 installed.
+Once you have python installed unzip simply launch
+```
+python entertainment_center.py
+```
+
+You should see your browser open up the application
--- media.py
+++ media.py
@@ -2,12 +2,13 @@ import webbrowser
 
 class Movie:
 
-	def __init__(self, movie_title, movie_storyline, 
-							poster_image, trailer_youtube):
+	# Constructor class. Initializes instances with given properties
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
+	# method that will open the class instance youtube video
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
