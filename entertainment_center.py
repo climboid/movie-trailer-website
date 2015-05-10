cff6503b935e82cd23c82ecca6d6d37c12ffbd5f
Oscar Villarreal <oscarvillarreal14@gmail.com>
2015-05-10 21:59:04 +0200

    adding more comments and passing the python code through a code formatter

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story of a boy that comes to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "http://fc02.deviantart.net/fs70/f/2010/014/b/c/Avatar_by_Eggar919.jpg", "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+# Create the instance variables that will containa all the necessary properties to render movies
 
-rambo = media.Movie("Rambo", "A soldier that suffers war", "http://i2.cdnds.net/13/36/618x400/rambo.jpg", "https://www.youtube.com/watch?v=OI0kenxkoNg")
+toy_story = media.Movie('Toy Story',
+                        'A story of a boy that comes to life',
+                        'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg'
+                        , 'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+
+avatar = media.Movie('Avatar', 'A marine on an alien planet',
+                     'http://fc02.deviantart.net/fs70/f/2010/014/b/c/Avatar_by_Eggar919.jpg'
+                     , 'https://www.youtube.com/watch?v=cRdxXPV9GNQ')
+
+rambo = media.Movie('Rambo', 'A soldier that suffers war',
+                    'http://i2.cdnds.net/13/36/618x400/rambo.jpg',
+                    'https://www.youtube.com/watch?v=OI0kenxkoNg')
+
+# Set those instance variables to the movies array
 
 movies = [toy_story, avatar, rambo]
 
+# Pass the movies array to the open_movies_page method which will open the youtube url in a modal window
+
 fresh_tomatoes.open_movies_page(movies)
+
+			
